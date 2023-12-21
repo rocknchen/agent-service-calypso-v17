bcae16b3b4cf3fa344899f3dd040cb86efb34601
rockchen <rochchen@htsc.com>
2023-12-21 09:14:18 +0800

    update

---
+++ src/main/java/com/hthk/calypso/cas/controller/CalypsoAgentServiceController.java
@@ -0,0 +1,43 @@
+package com.hthk.calypso.cas.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.hthk.fintech.controller.basic.AbstractController;
+import com.hthk.fintech.exception.ServiceInternalException;
+import com.hthk.fintech.exception.ServiceInvalidException;
+import com.hthk.fintech.exception.ServiceNotSupportedException;
+import com.hthk.fintech.model.data.datacenter.query.IDataCriteria;
+import com.hthk.fintech.model.web.http.HttpRequest;
+import com.hthk.fintech.model.web.http.HttpResponse;
+import com.hthk.fintech.model.web.http.HttpServiceRequest;
+import com.hthk.fintech.utils.HttpResponseUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.hthk.fintech.config.FintechStaticData.KW_HTTP_REQUEST;
+import static com.hthk.fintech.config.FintechStaticData.LOG_WRAP;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/20 17:16
+ */
+@RestController
+@RequestMapping("/")
+public class CalypsoAgentServiceController extends AbstractController {
+
+    private final static Logger logger = LoggerFactory.getLogger(CalypsoAgentServiceController.class);
+
+    @PostMapping(value = "/services")
+    public <P, C> HttpResponse<?> post(
+            @RequestBody HttpServiceRequest<P, C> request
+    ) throws JsonProcessingException, ServiceInvalidException, ServiceNotSupportedException, ServiceInternalException {
+
+        logger.info(LOG_WRAP, KW_HTTP_REQUEST, getDefaultObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(request));
+//        R result = dqmService.process(request);
+        return HttpResponseUtils.success(null);
+    }
+
+}
---
+++ src/main/java/com/hthk/calypso/cas/service/impl/DataQueryManagerDummyImpl.java
@@ -0,0 +1,22 @@
+package com.hthk.calypso.cas.service.impl;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.hthk.fintech.exception.ServiceInternalException;
+import com.hthk.fintech.exception.ServiceInvalidException;
+import com.hthk.fintech.exception.ServiceNotSupportedException;
+import com.hthk.fintech.model.data.datacenter.query.IDataCriteria;
+import com.hthk.fintech.model.web.http.HttpRequest;
+import com.hthk.fintech.service.DataQueryManagerService;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author: Rock CHEN
+ * @Date: 2023/12/20 18:47
+ */
+@Component
+public class DataQueryManagerDummyImpl implements DataQueryManagerService {
+    @Override
+    public <R> R process(HttpRequest<? extends IDataCriteria> request) throws ServiceInvalidException, JsonProcessingException, ServiceNotSupportedException, ServiceInternalException {
+        return null;
+    }
+}
